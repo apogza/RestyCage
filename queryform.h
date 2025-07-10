87f462f0088ce3fea2c75a86dbc8b0211f70d692
Apostol Bakalov <apogza@yahoo.com>
2025-07-10 13:55:25 +0300

    Move the query form to a separate widget

---
+++ queryform.h
@@ -0,0 +1,79 @@
+#ifndef QUERYFORM_H
+#define QUERYFORM_H
+
+#include <QWidget>
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
+#include <QStandardItemModel>
+#include <QTableView>
+#include <QNetworkReply>
+#include <QUrlQuery>
+#include "qjsonmodel.h"
+
+namespace Ui {
+class QueryForm;
+}
+
+class QueryForm : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit QueryForm(QWidget *parent = nullptr);
+    ~QueryForm();
+
+private slots:
+    void on_sendButton_clicked();
+    void on_authComboBox_currentIndexChanged(int index);
+    void on_reqAddParamBtn_clicked();
+    void on_reqRemoveParamBtn_clicked();
+    void on_reqHeadersAddBtn_clicked();
+    void on_reqHeadersRemoveBtn_clicked();
+    void on_reqBodyTypeComboBox_currentIndexChanged(int index);
+
+    void on_reqParamsTableView_doubleClicked(const QModelIndex &index);
+
+    void on_reqBodyFormDataAddRowBtn_clicked();
+
+    void on_reqHeadersTableView_doubleClicked(const QModelIndex &index);
+
+    void on_pushButton_2_clicked();
+
+    void on_reqAddUrlEncodedBodyRowBtn_clicked();
+
+    void on_reqRemoveUrlEncodedBodyRowBtn_clicked();
+
+    void on_reqUrlEncodedBodyTableView_doubleClicked(const QModelIndex &index);
+
+    void on_reqFileSelectionBtn_clicked();
+
+private:
+    Ui::QueryForm *ui;
+    QNetworkAccessManager *nam;
+    QStandardItemModel reqParamsModel;
+    QStandardItemModel reqHeadersModel;
+    QStandardItemModel reqUrlEncodedFormBodyModel;
+    QStandardItemModel reqFormBodyModel;
+    QString selectedBinaryBodyFilePath;
+    quint64 requestStartMs;
+
+    void initModels();
+    void addSimpleModelRow(QStandardItemModel &itemsModel);
+    void editSimpleRow(QStandardItemModel &itemsModel, int row, int column);
+    void removeModelRow(QTableView* tableView, QStandardItemModel &itemsModel);
+
+    void setRequestParams(QUrlQuery &url);
+    void setRequestAuth(QNetworkRequest &req);
+    void setRequestHeaders(QNetworkRequest &req);
+
+    void sendRequest(QNetworkRequest &request, QUrlQuery &urlQuery);
+    QNetworkReply *sendMultiPartRequest(QNetworkRequest &request, const QString &method);
+    QNetworkReply *sendUrlEncodedFormRequest(QNetworkRequest &request, const QString &method, QUrlQuery &urlQuery);
+    QNetworkReply *sendRawRequest(QNetworkRequest &request, const QString &method);
+    QNetworkReply *sendBinaryRequest(QNetworkRequest &request, const QString &method);
+
+
+    void readReply();
+    void readReplyHeaders(QNetworkReply* reply);};
+
+#endif // QUERYFORM_H
--- restycagewindow.h
+++ restycagewindow.h
@@ -2,12 +2,7 @@
 #define RESTYCAGEWINDOW_H
 
 #include <QMainWindow>
-#include <QNetworkAccessManager>
-#include <QNetworkRequest>
-#include <QStandardItemModel>
-#include <QTableView>
-#include <QNetworkReply>
-#include <QUrlQuery>
+#include "queryform.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -23,60 +18,9 @@ public:
     RestyCageWindow(QWidget *parent = nullptr);
     ~RestyCageWindow();
 
-private slots:
-    void on_sendButton_clicked();
-    void on_authComboBox_currentIndexChanged(int index);
-    void on_reqAddParamBtn_clicked();
-    void on_reqRemoveParamBtn_clicked();
-    void on_reqHeadersAddBtn_clicked();
-    void on_reqHeadersRemoveBtn_clicked();
-    void on_reqBodyTypeComboBox_currentIndexChanged(int index);
-
-    void on_reqParamsTableView_doubleClicked(const QModelIndex &index);
-
-    void on_reqBodyFormDataAddRowBtn_clicked();
-
-    void on_reqHeadersTableView_doubleClicked(const QModelIndex &index);
-
-    void on_pushButton_2_clicked();
-
-    void on_reqAddUrlEncodedBodyRowBtn_clicked();
-
-    void on_reqRemoveUrlEncodedBodyRowBtn_clicked();
-
-    void on_reqUrlEncodedBodyTableView_doubleClicked(const QModelIndex &index);
-
-    void on_reqFileSelectionBtn_clicked();
-
 private:
     Ui::RestyCageWindow *ui;
-    QNetworkAccessManager *nam;
-    QStandardItemModel reqParamsModel;
-    QStandardItemModel reqHeadersModel;
-    QStandardItemModel reqUrlEncodedFormBodyModel;
-    QStandardItemModel reqFormBodyModel;
-    QString selectedBinaryBodyFilePath;
-    quint64 requestStartMs;
-
-    void initModels();
-    void addSimpleModelRow(QStandardItemModel &itemsModel);
-    void editSimpleRow(QStandardItemModel &itemsModel, int row, int column);
-    void removeModelRow(QTableView* tableView, QStandardItemModel &itemsModel);
-
-    void setRequestParams(QUrlQuery &url);
-    void setRequestAuth(QNetworkRequest &req);
-    void setRequestHeaders(QNetworkRequest &req);
-
-    void sendRequest(QNetworkRequest &request, QUrlQuery &urlQuery);
-    QNetworkReply *sendMultiPartRequest(QNetworkRequest &request, const QString &method);
-    QNetworkReply *sendUrlEncodedFormRequest(QNetworkRequest &request, const QString &method, QUrlQuery &urlQuery);
-    QNetworkReply *sendRawRequest(QNetworkRequest &request, const QString &method);
-    QNetworkReply *sendBinaryRequest(QNetworkRequest &request, const QString &method);
-
-
-    void readReply();
-    void readReplyHeaders(QNetworkReply* reply);
-
+    QueryForm *queryForm;
 
 };
 #endif // RESTYCAGEWINDOW_H
