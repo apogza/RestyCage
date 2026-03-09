71fc084eb2382a538ac253e14efdcf4346c5dce1
Apostol Bakalov <apogza@gmail.com>
2026-03-09 22:13:32 +0200

    Make the db part fo the current directory

--- constants.h
+++ constants.h
@@ -5,12 +5,13 @@
 #include <QByteArray>
 #include <QGlobalStatic>
 #include <QStandardPaths>
+#include <QDir>
 
 const QString settingsOrgKey = "NoOrg";
 const QString settingsAppKey = "RestyCage";
 const QString activeEnvironmentId = "activeEnvironmentId";
 
-const QString appDataFolder = QString("%1/%2").arg(QStandardPaths::writableLocation(QStandardPaths::AppDataLocation), "resty_cage");
+const QString appDataFolder = QString("%1/%2").arg(QDir::currentPath(), "resty_cage");
 const QString dbPath = QString("%1/%2").arg(appDataFolder, "resty_cage.db");
 const QString collectionsTable = "collections";
 const QString queriesTable = "queries";
